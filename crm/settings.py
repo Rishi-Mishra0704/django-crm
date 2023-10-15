d7c9587cf2d4b2ab9b4f09fa56074165c543bcee
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-16 00:24:41 +0530

    update hosts

--- crm/settings.py
+++ crm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+f2&nq)!r^&#+h%eihyg^ej66v&m=b*wt*vi)v&69bm1(l=qic
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
